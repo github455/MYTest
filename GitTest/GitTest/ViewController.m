52702c59529cfce677acce6e1a64c1f749bde93f
demon <123@liujin.com>
2015-12-01 10:06:13 +0800

    Test

---
+++ GitTest/GitTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GitTest
+//
+//  Created by andy@LA on 15/12/1.
+//  Copyright © 2015年 tarena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ GitTest/GitTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitTest
+//
+//  Created by andy@LA on 15/12/1.
+//  Copyright © 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
